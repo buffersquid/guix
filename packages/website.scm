e65f983464055295d3a8f6d89b8191f78b019fab
buffersquid <mail@buffersquid.com>
2025-06-24 23:04:26 -0700

    Add files for initial guix build

---
+++ packages/website.scm
@@ -0,0 +1,18 @@
+(define-module (packages website)
+  #:use-module (guix packages)
+  #:use-module (guix gexp)
+  #:use-module (guix build-system copy)
+  #:use-module (gnu services)
+  #:use-module (gnu services web)
+  #:export (website))
+
+(define-public website
+  (package
+    (name "website")
+    (version "0.0")
+    (source (local-file "../src" #:recursive? #t))
+    (build-system copy-build-system)
+    (synopsis "Simple static site")
+    (description "A single-page static website")
+    (home-page "")
+    (license #f)))
